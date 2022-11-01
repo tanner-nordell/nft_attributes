6685c835c884d89b0534ba73aa12169aa6a3e1e4
tanner-nordell <hnordell@seas.upenn.edu>
2022-10-31 22:18:33 -0400

    worked with my dog snoop

--- get_ape_info.py
+++ get_ape_info.py
@@ -5,6 +5,8 @@ import requests
 import json
 import time
 
+import re
+
 bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
 contract_address = Web3.toChecksumAddress(bayc_address)
 
@@ -12,8 +14,9 @@ contract_address = Web3.toChecksumAddress(bayc_address)
 #The file 'abi.json' has the ABI for the bored ape contract
 #In general, you can get contract ABIs from etherscan
 #https://api.etherscan.io/api?module=contract&action=getabi&address=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D
-with open('/home/codio/workspace/abi.json', 'r') as f:
+with open('/Users/tannernordell/Documents/UPenn MCIT Graduate Program/CIT 582 - Fall 2022/nft_attributes/abi.json', 'r') as f:
 	abi = json.load(f) 
+#/home/codio/workspace/abi.json
 
 ############################
 #Connect to an Ethereum node
@@ -25,10 +28,25 @@ def get_ape_info(apeID):
 	assert isinstance(apeID,int), f"{apeID} is not an int"
 	assert 1 <= apeID, f"{apeID} must be at least 1"
 
-	data = {'owner': "", 'image': "", 'eyes': "" }
+
 	
 	#YOUR CODE HERE
-	Contract.
+	bayc_inst = web3.eth.contract(address=bayc_address, abi=abi)
+	#total_supply = bayc_inst.functions.totalSupply().call()
+	owner = bayc_inst.functions.ownerOf(apeID).call()
+	ape_URI = bayc_inst.functions.tokenURI(apeID).call()
+
+	token = re.match('ipfs://(.*)', ape_URI).group(1)
+	url = f'https://gateway.pinata.cloud/ipfs/{token}'
+	res = requests.get(url)
+	res_json = res.json()
+
+	image = res_json['image']
+	eyes = [x['value'] for x in res_json['attributes'] if x['trait_type'] == 'Eyes'][0]
+
+	data = {'owner': owner, 'image': image, 'eyes': eyes}
+
+	#tokenURI
 
 	assert isinstance(data,dict), f'get_ape_info{apeID} should return a dict' 
 	assert all( [a in data.keys() for a in ['owner','image','eyes']] ), f"return value should include the keys 'owner','image' and 'eyes'"
